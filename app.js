5bf1c73256ee628111aa667ff2e412eb436e97cc
Myroslav <muros2019icloud@gmail.com>
2022-03-06 22:18:18 +0100

    Випадаючий список(мови)

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 
 
+$(document).ready(function(){
+    // Add smooth scrolling to all links
     $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            event.preventDefault()
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
             var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        }
+        } // End if
     });
+});
 
 
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
